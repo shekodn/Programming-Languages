af92e532d50a14eaa906d651d6b0227b70f790c3
shekodn <sdiazn19@gmail.com>
2017-03-14 23:26:10 -0600

    adds entrega

--- homeworks/4/6.rkt
+++ homeworks/4/6.rkt
@@ -1,30 +1,7 @@
-
-#lang racket
-(define g
-            '((A (B 2) (D 10))
-              (B (C 9) (E 5))
-                (C (A 12) (D 6))
-                  (D (E 7))
-                    (E (C 3))
-))
-
-
-
-;counts how many elements are in a list
-(define (suma list)
-  (if (null? list)
-      -1
-      (+ 1 (suma (cdr list)))
-  )
-)
-
-
-;list of legths: buil a new list with the lengths of each list on graphs ;excluding the grpah itself
-(define (list-of-leghts lists)
-  (if (null? lists)
-      '()
-      (cons (suma (car lists))
-            (list-of-leghts (cdr lists))))
-)
-
-(list-of-leghts g)
+;6
+;función retorna los nodos que tienen n destinos
+; recibe como parametros el grafo y los destinos
+(define (n-destinos g n); se hace un map del grafo, adentro una función lambda para que reciba cada nodo y sus arcos
+  (flatten (map (lambda (x) (cond ((= n (apply + (map (lambda (y) 1) (cdr x))) ) (car x)); se hace un appply +  para sumatoria del numero de arcos
+                          (else '()))) g)); si la sumatoria de arcos es igual a n se agregase retorna el nodo para la lista resultante de map
+); si no se regresa una lista vacía, al final se usa flatten para eliminar las listas vacías dentro del resultado
--- homeworks/4/A01230264_tuMatricula_Tarea4.rkt
+++ homeworks/4/A01230264_tuMatricula_Tarea4.rkt
@@ -180,7 +180,13 @@
 ;tienen N nodos destino. No se permite usar la función primitiva length.
 
 
-
+;6
+;función retorna los nodos que tienen n destinos
+; recibe como parametros el grafo y los destinos
+(define (n-destinos g n); se hace un map del grafo, adentro una función lambda para que reciba cada nodo y sus arcos
+  (flatten (map (lambda (x) (cond ((= n (apply + (map (lambda (y) 1) (cdr x))) ) (car x)); se hace un appply +  para sumatoria del numero de arcos
+                          (else '()))) g)); si la sumatoria de arcos es igual a n se agregase retorna el nodo para la lista resultante de map
+); si no se regresa una lista vacía, al final se usa flatten para eliminar las listas vacías dentro del resultado
 
 
 
