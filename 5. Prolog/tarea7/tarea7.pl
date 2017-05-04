48fb1d072f3cad02cc8083544096c9360f72fe43
shekodn <sdiazn19@gmail.com>
2017-05-03 22:15:04 -0500

    refactors

--- 5. Prolog/tarea7/tarea7.pl
+++ 5. Prolog/tarea7/tarea7.pl
@@ -77,11 +77,33 @@ intersectan([X|Y],[W|Z]):- member(X, [W|Z]), !.
 intersectan([X|Y],[W|Z]):- member(W, [X|Y]), !.
 intersectan([X|Y],[W|Z]):- intersectan(Y,Z).
 
+
 % Problema 3
 % rango utiliza una función auxiliar para agregar los valores a la lista a la lista
 rango(N, M, R):- auxRango(N, M, [], R).
 auxRango(N, M, L, R):- N =:= M, append(L, [N], D), =(R, D), !.
 auxRango(N, M, L, R):- N =\= M, append(L, [N], D), N1 is N+1, auxRango(N1, M, D, R).
+
+% Problema 4
+% Programa que hace el producto cartesiano de dos listas
+cartesiano(L1, L2, R):- auxCartesiano(L1, L2, [], R).
+auxCartesiano([], _, P, R):- =(R, P), !.
+auxCartesiano(_, [], P, R):- =(R, P), !.
+auxCartesiano([X|Y], L2, P, R):- agrega(X, L2, [], D), append(P, D, L),auxCartesiano(Y, L2, L, R).
+% funcion auxiliar que hace el producto de un atomo con todos los elementos de una listima
+agrega(N, [], D, L):-  =(L, D), !.
+agrega(N, [X|Y], D, L):- append(D, [[N,X]], R), agrega(N, Y, R, L).
+
+
+% Problema 5
+% programa que cuenta las veces que aparece un número en una lista imbricada
+cuenta_profundo(N,L,R):- auxCuenta(N, L, 0, R).
+% función auxiliar que cunado el primer elemento de la lista es igual a N suma 1 al contador
+% si el primer elemento es otra lista entonces llama a la función con ese elemento y con el resto
+auxCuenta(N, [], M, R):- R is M, !.
+auxCuenta(N, [X|Y], M, R):- N == X, M1 is M+1, auxCuenta(N, Y, M1, R), !.
+auxCuenta(N, [[X|Y]|Z], M, R):- auxCuenta(N, [X|Y], M, L), auxCuenta(N, Z, L, R), !.
+auxCuenta(N, [X|Y], M, R):- auxCuenta(N, Y, M, R).
 
 
 % 6. (10 puntos) Implementar el predicado tabla en Prolog que obtenga la tabla
@@ -96,26 +118,33 @@ auxTabla(N, M, L, R):- N =:= 10, NM is N*M, append(L, [[[M,N], NM]], D), =(R, D)
 auxTabla(N, M, L, R):- N =\= 10, NM is N*M, append(L, [[[M,N], NM]], D), N1 is N+1, auxTabla(N1, M, D, R).
 
 
-% 7. Implementar el predicado lista_unicos en Prolog que obtenga una
-% lista con los elementos que no aparecen repetidos dentro de una lista imbricada.
+% Problema 7
+% hace una lista con los elementos únicos en una lista
+lista_unicos(L, R) :- auxLista_unicos(L, [], [], R).
+% si es lista mandas llamar auxLista_unicos(Y, L, R).
+% si es atomo metes a la lista
+member(X, [[W|Y]|T]) :- member(X, [W|Y]), !.
+member(X, [[W|Y]|T]) :- member(X, T), !.
+member(X, [Y|T]) :- X = Y; member(X, T).
+auxLista_unicos([], U, E, R):- =(R, U), !.
 
-lista_unicos(L, R) :- auxLista_unicos(L, [], R).
-auxLista_unicos([], U, R):- =(R, U), !.
-%si es lista mandas llamar auxLista_unicos(Y, L, R).
-%si es atomo metes a la lista
-auxLista_unicos([X|Y], U, R):- member(X, Y), auxLista_unicos(Y, U, R), !.
-auxLista_unicos([X|Y], U, R):- append(U, [X], L), auxLista_unicos(Y, L, R).
+auxLista_unicos([[X|Y]|Z], U, E, R):- auxLista_unicos([X|Y], U, E, D), auxLista_unicos(Z, D, E, R), !.
+auxLista_unicos([X|Y], U, E, R):- member(X, E), !, auxLista_unicos(Y, U, E, R).
+auxLista_unicos([X|Y], U, E, R):- member(X, Y), !, append(E, [X], D), auxLista_unicos(Y, U, D, R).
+auxLista_unicos([X|Y], U, E, R):- append(U, [X], L), auxLista_unicos(Y, L, E, R).
+
 
 %8 mayores
-mayores(N,Arbol,H) :- mayoresAux(N,Arbol,H).
-mayoresAux(N, nil, H) :- =(H, nil), !.
-
+mayores(N,Arbol,H) :- mayoresAux(N,Arbol,R, H).
+mayoresAux(N, nil, R, H) :- =(H, nil), !.
+mayoresAux(N, arbol(Raiz,Left,Right),R, H) :-  Raiz >= N, append(R,[Raiz],D),mayoresAux(N,Left,D,E), mayoresAux(N,Right,E,H),=(H, E), !.
+mayoresAux(N, arbol(Raiz,Left,Right),R, H) :- mayoresAux(N,Left,D,E), mayoresAux(N,Right,E,H), =(H, E), !.
 
 
 % 9
 % Agrega a arbol
-siembraAux(X,nil,arbol(X,nil,nil)).
-siembraAux(X,arbol(Raiz,L,R),arbol(Raiz,L1,R)) :- X @< Raiz, siembraAux(X,L,L1).
+siembraAux(X, nil, arbol(X,nil,nil)).
+siembraAux(X, arbol(Raiz,L,R), arbol(Raiz,L1,R)) :- X @< Raiz, siembraAux(X,L,L1).
 siembraAux(X,arbol(Raiz,L,R),arbol(Raiz,L,R1)) :- X @> Raiz, siembraAux(X,R,R1).
 
 % Funcion siembra que construye arbol
